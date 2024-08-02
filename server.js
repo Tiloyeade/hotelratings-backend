85300228f317214ba1501f32510c498ff357ac50
unknown <adegoroyeadetiloye@gmail.com>
2024-08-02 17:21:35 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/hotel-ratings', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const RatingSchema = new mongoose.Schema({
+    reservation: Number,
+    facilities: Number,
+    customerService: Number,
+    remarks: String,
+});
+
+const Rating = mongoose.model('Rating', RatingSchema);
+
+app.post('/api/ratings', async (req, res) => {
+    const { reservation, facilities, customerService, remarks } = req.body;
+    const rating = new Rating({ reservation, facilities, customerService, remarks });
+    await rating.save();
+    res.send(rating);
+});
+
+app.get('/api/ratings', async (req, res) => {
+    const ratings = await Rating.find();
+    res.send(ratings);
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
